eca47833d67a35e36ac96c16dc57a5099af8aadb
Thomas Bowers <thomas.bowers@durham.ac.uk>
2019-01-15 12:12:38 +0000

    Restructured a bit and added getters and setters also a way to regen points alowing trees to be moved

--- sketch.js
+++ sketch.js
@@ -1,18 +1,60 @@
+let trees = [];
+let num_trees = 20;
+let color = ['red', 'green', 'blue', 'greyscale'];
+let c_num;
+let bLength;
+let x;
+let w;
+
 class Tree {
     constructor(levels, drawSpeed, color, position, bLength) {
-        //frameRate(drawSpeed);
-        this.levels = levels;
         this.points = [];
-        this.color = color;
-        this._drawLevel = this.levels;
+        this._color = color;
+        this._levels = levels;
+        this._bLength = bLength;
+        this._position = position;
         this._initialPos = createVector(position, height);
-        this.bLength = bLength;
-        for (let i = 0; i <= this.levels; i++) {
+        this.changeUpdate();
+        this.drawY(this._levels, 0, this._initialPos);
+    }
+    changeUpdate(){
+        this.points = [];
+        for (let i = 0; i <= this._levels; i++) {
             this.points.push([]);
         }
+        this.drawY(this._levels, 0, this._initialPos);
+    }
 
-        this.drawY(this.levels, 0, this._initialPos);
 
+    get position() {
+        return this._position;
+    }
+    set position(value) {
+        this._position = value;
+        this.changeUpdate();
+    }
+    get bLength() {
+        return this._bLength;
+    }
+    set bLength(value) {
+        this._bLength = value;
+        this.changeUpdate();
+
+    }
+    get levels() {
+        return this._levels;
+    }
+    set levels(value) {
+        this._levels = value;
+        this.changeUpdate();
+
+    }
+    get color() {
+        return this._color;
+    }
+
+    set color(value) {
+        this._color = value;
     }
 
     drawY(level, angle, lastPos) {
@@ -21,7 +63,7 @@ class Tree {
         if (level < 0) return;
 
         // create vector, and add onto last location of branch
-        let currentPos = createVector(0, -level * this.bLength).rotate(angle).add(lastPos);
+        let currentPos = createVector(0, -level * this._bLength).rotate(angle).add(lastPos);
 
         // other base case, if branch is under ground
         if (currentPos.y > height) return;
@@ -43,26 +85,30 @@ class Tree {
 
     draw() {
         // go through the array and draw level
-        for (let v = this.levels; v > 0; v--) {
+        for (let v = this._levels; v > 0; v--) {
             for (let c = 0; c < this.points[v].length; c++) {
                 strokeWeight(v);
-                if (this.color == 'red') {
-                    stroke(map(v, 0, this.levels, 250, 0), 0, 0);
-                }
-                else if (this.color == 'green') {
-                    stroke(0, map(v, 0, this.levels, 250, 0), 0);
-                }
-                else if (this.color == 'blue') {
-                    stroke(0, 0, map(v, 0, this.levels, 250, 0));
-                }
-                else if (this.color == 'greyscale') {
-                    stroke(map(v, 0, this.levels, 250, 0))
-                }
 
+                if (this._color == 'red') {
+                    stroke(map(v, 0, this._levels, 250, 0), 0, 0);
+                }
+                else if (this._color == 'green') {
+                    stroke(0, map(v, 0, this._levels, 250, 0), 0);
+                }
+                else if (this._color == 'blue') {
+                    stroke(0, 0, map(v, 0, this._levels, 250, 0));
+                }
+                else if (this._color == 'greyscale') {
+                    stroke(map(v, 0, this._levels, 250, 0))
+
+                }
                 line(this.points[v][c].l.x, this.points[v][c].l.y, this.points[v][c].c.x, this.points[v][c].c.y);
+                }
             }
         }
-    }
+
+    
+
 
 }
 
@@ -73,28 +119,18 @@ function removeAll() {
 }
 
 function removeLast() {
-    trees.pop()
+    trees.pop();
     clear();
     setup();
 }
 
 function removeRandom() {
     let s = Math.floor(Math.random() * trees.length);
-    trees.splice(s, 1)
+    trees.splice(s, 1);
     clear();
     setup();
 }
 
-function forest() {
-    x = 0;
-    num_trees = parseInt(document.getElementById("numTrees").value);
-    while (x < num_trees) {
-        c_num = Math.floor(Math.random() * color.length);
-        bLength = Math.floor(Math.random() * 6) + 4;
-        trees.push(new Tree(12, 60, color[c_num], width * Math.random(), bLength));
-        x++;
-    }
-}
 function addTrees() {
     let randomSpacing = document.getElementById("randomSpacing").checked;
     let variedHeight = document.getElementById("variedHeight").checked;
@@ -116,46 +152,9 @@ function addTrees() {
         trees.push(new Tree(12, 60, color[c_num], spacing, bLength));
         x++;
     }
+    console.log('added')
+    console.log(trees)
 }
-/*
-function rTree() {
-    bLength = Math.floor(Math.random() * 6) + 4;
-    fW = width * Math.random();
-    trees.push(new Tree(12, 60, color[0], fW, bLength));
-    trees.push(new Tree(12, 60, color[1], fW, bLength));
-    trees.push(new Tree(12, 60, color[2], fW, bLength));
-    trees.push(new Tree(12, 60, color[3], fW, bLength));
-    trees.push(new Tree(12, 60, color[3], fW, bLength));
-    trees.push(new Tree(12, 60, color[1], fW, bLength));
-    trees.push(new Tree(12, 60, color[2], fW, bLength));
-    trees.push(new Tree(12, 60, color[0], fW, bLength));
-}
-*/
-function tree() {
-    c_num = Math.floor(Math.random() * color.length);
-    trees.push(new Tree(12, 60, color[c_num], (width / 2), 5));
-
-}
-
-function orchard() {
-    num_trees = parseInt(document.getElementById("numTrees").value);
-    x = 0;
-    c_num = Math.floor(Math.random() * color.length);
-    bLength = Math.floor(Math.random() * 6) + 4;
-    while (x < num_trees) {
-        trees.push(new Tree(12, 60, color[c_num], ((w * 8 / (num_trees - 1)) * x + w), bLength));
-        x++;
-    }
-}
-
-let trees = [];
-let num_trees = 20;
-//let type = 3;
-let color = ['red', 'green', 'blue', 'greyscale'];
-let c_num;
-let bLength;
-let x;
-let w;
 
 
 function setup() {
@@ -163,38 +162,26 @@ function setup() {
     canvas.parent('sketch-holder');
     canvas.background("#00bfff");
     w = width * 0.1;
+    colorMode(RGB,255)
 }
 
 function draw() {
-    if (trees != null) {
-        for (let i = 0; i < trees.length; i++) {
-            trees[i].draw();
+        if (trees != null) {
+            for (let i = 0; i < trees.length; i++) {
+                trees[i].draw();
+            }
         }
-    }
-    else {
-        console.log("No trees")
-    }
+        else {
+            console.log("No trees")
+        }
+    
 }
-/*
-function keyTyped() {
-    if (key === 'r') {
-        removeAll();
-    }
-    else if (key === 't') {
-        tree();
-    } else if (key === 'o') {
-        orchard();
-    } else if (key === 'f') {
-        forest();
-    } else if (key === 'c') {
-        removeLast();
-    }
 
-}
-*/
+
 function windowResized() {
     canvas.width = windowWidth * 0.8;
     canvas.height = windowHeight * 0.8;
+    change = 1
     setup();
 }
 
